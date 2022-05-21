72979d315b4b003274b6e554d677396390a72a07
Devon Wasson <devonwasson@gmail.com>
2022-05-21 00:55:55 +0000

    the bot now tags people in a message when they get a new role. As such, the bot now also ignores messages that it sends when scraping for levels

--- reward_role.py
+++ reward_role.py
@@ -32,8 +32,16 @@ if last_message_id != "":
 else:
 	messages = discord_helper.send_request(discord_helper.GET, scrapeURL+"?limit=100", constants.headers).json()
 for message in messages:
+	# Store the first message's ID as this round's most recent message ID
 	if most_recent_message_id == "":
 		most_recent_message_id = message['id']
+	# If we've already seen this message, end early
+	if last_message_id == most_recent_message_id:
+		break
+	# Ignore messages by this bot
+	if message['author']['id'] == TOKENS['bot_id']:
+		continue
+	# Check the message
 	user_id = message['mentions'][0]['id']
 	level = message['content'].split("level ")[1].split("!")[0]
 	if level in role_ids:
@@ -42,7 +50,8 @@ for message in messages:
 		discord_helper.send_request(discord_helper.PUT, roleURL.format(user_id, new_role_id), constants.headers)
 		if last_role_id != "":
 			discord_helper.send_request(discord_helper.DELETE, roleURL.format(user_id, last_role_id), constants.headers)
-
+		message_data = {'content': "Congratulations on reaching level " + level + ", <@"+user_id+">! Take a look at your profile to see your shiny new rank!"}
+		discord_helper.send_request(discord_helper.POST, scrapeURL, constants.headers, json.dumps(message_data))
 if most_recent_message_id == "":
 	most_recent_message_id = last_message_id
 constants.set_last_message_id(most_recent_message_id)
